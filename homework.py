b11af571ec0b449cd3032baf0a00267235ea1df7
Stepan Pryn <prynstepan@yandex.ru>
2020-11-01 17:33:47 +0300

    hw 01 API 2nd trying

--- homework.py
+++ homework.py
@@ -3,32 +3,39 @@ import requests
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
+import logging
 load_dotenv()
 
-account_sid = os.getenv('ACCOUNT_SID')
-auth_token = os.getenv('AUTH_TOKEN')
-number_to = os.getenv('NUMBER_TO')
-number_from = os.getenv('NUMBER_FROM')
-access_token = os.getenv('ACCESS_TOKEN')
-client = Client(account_sid, auth_token)
+ACCOUNT_SID = os.getenv('ACCOUNT_SID')
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+NUMBER_TO = os.getenv('NUMBER_TO')
+NUMBER_FROM = os.getenv('NUMBER_FROM')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+URL_API_VK = "https://api.vk.com/method/"
+TWILIO_CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 
 def get_status(user_id):
     params = {
         'user_ids': user_id,
-        'access_token': access_token,
+        'access_token': ACCESS_TOKEN,
         'fields': 'online',
         'v': 5.92
     }
-    status = requests.post("https://api.vk.com/method/users.get", params=params)
-    user_info = status.json()['response']
-    return (user_info[0]['online'])
+    method = 'users.get'
+    url = URL_API_VK + method
+    try:
+        status = requests.post(url, params=params)
+        user_info = status.json()['response']
+        return user_info[0]['online']
+    except Exception as error:
+        logging.exception('Exception occurred')
 
 
 def sms_sender(sms_text):
-    message = client.messages.create(
-        to=number_to, 
-        from_=number_from,
+    message = TWILIO_CLIENT.messages.create(
+        to=NUMBER_TO, 
+        from_=NUMBER_FROM,
         body=sms_text)
     return message.sid
 
@@ -36,7 +43,11 @@ def sms_sender(sms_text):
 if __name__ == '__main__':
     vk_id = input('Введите id ')
     while True:
-        if get_status(vk_id) == 1:
+        if get_status(vk_id) == 0:
+            logging.error(f'{vk_id} сейчас оффлайн!')
+        elif get_status(vk_id) == 1:
             sms_sender(f'{vk_id} сейчас онлайн!')
             break
+        else:
+            logging.error('"get_status" function raised an error!')
         time.sleep(5)
